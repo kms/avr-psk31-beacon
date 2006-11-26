5daf8660321bcfc1a45caf6dda44ff1e1e0728ad
Karl-Martin Skontorp <kms@skontorp.net>
2006-11-26 22:50:43 +0000

    git-svn-id: svn+ssh://ginandtonic.no/home/kms/.svnrepos/avr-psk31-beacon@655 bec0a6a6-59ba-0310-824c-86e0bd1f4ed1

--- avr-psk31-beacon.c
+++ avr-psk31-beacon.c
@@ -4,7 +4,10 @@
  */
 
 #define F_CPU 12E6
-#define SIN_LUT_SIZE 64
+#define BB_F 732
+#define SIN_LUT_SIZE 32
+//#define SYM_PERIOD ((0.032 * SIN_LUT_SIZE) / (1.0 / BB_F))
+#define SYM_PERIOD 1500
 
 #include <math.h>
 #include <stdlib.h>
@@ -20,28 +23,55 @@ volatile uint16_t p;
 
 uint8_t txChar(void);
 
-int8_t sinLUT[SIN_LUT_SIZE];
+int8_t sinLUT[SIN_LUT_SIZE][16];
 
 ISR(SIG_OUTPUT_COMPARE0A) {
-    if (p++ == (1472)) {
-	p = 0;
+    static uint8_t shift;
 
+    p++;
+
+    if (p == SYM_PERIOD / 2) {
 	if (txChar() == 0) {
-	    i += 32;
+	    shift = 1;
+	} else {
+	    shift = 0;
+	}
+
+	if (PORTD & _BV(PIND0)) {
+	    PORTD &= ~(_BV(PIND0));
+	} else {
+	    PORTD |= _BV(PIND0);
+	}
+    } else if (p == SYM_PERIOD) {
+	p = 0;
+	if (shift) {
+	    i += SIN_LUT_SIZE / 2;
 	}
     }
 
-    int16_t x = sinLUT[i++ % SIN_LUT_SIZE];
+    int16_t x;
+
+    if (shift) {
+	if (p < SYM_PERIOD / 2) {
+	    x = sinLUT[i++ % SIN_LUT_SIZE][12 - (p / 128)];
+	} else {
+	    x = sinLUT[i++ % SIN_LUT_SIZE][p / 128];
+	}
+    } else {
+	x = sinLUT[i++ % SIN_LUT_SIZE][0];
+    }
+
     x += 128;
     OCR0A = (uint8_t) x;
 }
 
 uint8_t txChar() {
     static char txString[] = 
+	"\t\t\t"
 	"\t\t... LA9PMA/B LA9PMA/B ...\r\n"
 	"\t\tLA9PMA/B < JO59fe Tonsberg >\r\n"
-//	"\t\tLA9PMA/B < 5W in dipole >\r\n"
-//	"\t\tLA9PMA/B < 28.321.732MHz >\r\n"
+	//	"\t\tLA9PMA/B < 5W in dipole >\r\n"
+	//	"\t\tLA9PMA/B < 28.321.732MHz >\r\n"
 	"\t\tLA9PMA/B < Op: kms@skontorp.net  +47 952 47 971 >\r\n"
 	"\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t";
     static char *txStringPos = txString;
@@ -75,13 +105,16 @@ uint8_t txChar() {
 
 void prepareSinLUT() {
     uint8_t l;
+    uint8_t m;
 
     for (l = 0; l < SIN_LUT_SIZE; l++) {
 	double d;
 
-	d = sin((((((double) l) / SIN_LUT_SIZE)) - 0.5) * M_PI * 2.0);
-	d *= 127.0;
-	sinLUT[l] = d;
+	for (m = 0; m < 16; m++) {
+	    d = sin((((((double) l) / SIN_LUT_SIZE)) - 0.5) * M_PI * 2.0) * cos(((double) m)/24.0 * M_PI);
+	    d *= 127.0;
+	    sinLUT[l][m] = d;
+	}
     }
 }
 
@@ -96,12 +129,13 @@ int main(void) {
     TIMSK0 = _BV(OCIE0A);
 
     DDRD = _BV(PIND6);
+    DDRD |= _BV(PIND0);
 
     set_sleep_mode(SLEEP_MODE_IDLE);
 
     sei();
 
     for (;;) {
-        sleep_mode();
+	sleep_mode();
     }
 }
